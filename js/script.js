8955f6bad93b50d756b1192253dd401c2eab50bc
In-memory <phrangboklang@gmail.com>
2026-08-02 13:42:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
+const tbody = document.getElementById("lectureBody");
 
-fetch('data/lectures.json')
-.then(r=>r.json())
-.then(data=>{
- const tb=document.querySelector('#lectureTable tbody');
- data.forEach(l=>{
-   tb.innerHTML+=`<tr>
-   <td>${l.lecture}</td>
-   <td>${l.date}</td>
-   <td>${l.topic}</td>
-   <td><a class="btn" target="_blank" href="${l.slides}">Download</a></td>
-   <td>${l.reference||'-'}</td>
-   </tr>`;
- });
+fetch("data/lectures.json")
+.then(res => res.json())
+.then(data => {
+
+    if(data.length===0){
+
+        tbody.innerHTML = `
+        <tr>
+            <td colspan="5" class="placeholder">
+                No lectures have been uploaded yet.
+            </td>
+        </tr>`;
+        return;
+    }
+
+    data.forEach((lec,index)=>{
+
+        tbody.innerHTML += `
+        <tr>
+            <td>${index+1}</td>
+            <td>${lec.date}</td>
+            <td>${lec.topic}</td>
+            <td><a class="button" href="${lec.slides}" target="_blank">Download</a></td>
+            <td>${lec.reference || "-"}</td>
+        </tr>`;
+
+    });
+
 });
